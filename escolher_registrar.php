323063c547bbf7329a2880657c629a959cc29b1b
GLeoni17 <gabriel.lduarte@outlook.com>
2020-10-14 21:54:44 -0300

    1a versao, sem dropdown

---
+++ escolher_registrar.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Escolha o que registrar</title>
+
+    <link rel="icon" href="./images/icon.svg" />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;800&display=swap"
+      rel="stylesheet"
+    />
+    <link href="bootstrap-4.4.1-dist/css/bootstrap.min.css" type="text/css" rel="stylesheet" />
+</head>
+<body>
+    <div class="container">
+        <div class="genero">
+
+        </div>
+        <div class="banda">
+            
+        </div>
+    </div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,59 @@
+<!--
+    Ícones feitos por <a href="https://www.flaticon.com/br/autores/dinosoftlabs" title="DinosoftLabs">DinosoftLabs</a> from <a href="https://www.flaticon.com/br/" title="Flaticon"> www.flaticon.com</a>
+    Ícones feitos por <a href="http://www.freepik.com/" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/br/" title="Flaticon"> www.flaticon.com</a>
+    https://br.freepik.com/vetores-gratis/silhuetas-de-pessoas-dancando_1178107.htm
+-->
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>MusicPlayer</title>
+    <script src="./jquery-3.5.1.min.js"></script>
+    <link rel="icon" href="./images/icon.svg" />
+
+
+    <link rel="stylesheet" href="./css/main.css" />
+    <link rel="stylesheet" href="./css/animations.css" />
+    <link rel="stylesheet" href="./css/page-landing.css" />
+    
+
+    <link
+      href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;800&display=swap"
+      rel="stylesheet"
+    />
+    
+    
+    
+  </head>
+  <body>
+    <div id="page-landing">
+      <div class="container">
+
+        <main>
+          <h1 class="animate-up espaco_esquerda">
+            Seja bem vindo ao MusicPlayer
+          </h1>
+
+          <section class="visit">
+            <p class="animate-up espaco_esquerda">Solta o play na sua playlist!</p>
+
+            <!--href="escolher_registrar.php"-->
+
+            <a
+              
+              title="Tocar playlist"
+              class="animate-up"
+            >
+              <img
+                id="botao-play"
+                src="./images/botao-play.svg"
+                alt="Tocar a playlist"
+              />
+            </a>
+          </section>
+        </main>
+      </div>
+    </div>
+  </body>
+</html>
