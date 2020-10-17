866f36c5d62cf92848b61d8a04ea0941892099af
GLeoni17 <gabriel.lduarte@outlook.com>
2020-10-17 20:52:58 -0300

    Nova versao - Rodando video do youtube com o final embed

--- listar_musica.php
+++ listar_musica.php
@@ -1,3 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Listar Playlists</title>
+</head>
+<body>
+    <a href="escolher_registrar.php"><img src="./images/seta_esquerda.png" alt="Retornar ao menu" width="20px" height="20px">Voltar ao menu</a><br><br>
 <?php
     include "conexao.php";
     $select = "SELECT musica.nome as nome_musica, banda.nome as nome_banda, genero.nome as nome_genero FROM musica
@@ -10,3 +19,5 @@
         echo "<b>".$row["nome_genero"]."</b> |";
     }
 ?>
+</body>
+</hmtl>
--- listar_playlist.php
+++ listar_playlist.php
@@ -1,9 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Listar Playlists</title>
+</head>
+<body>
+    <a href="escolher_registrar.php"><img src="./images/seta_esquerda.png" alt="Retornar ao menu" width="20px" height="20px">Voltar ao menu</a><br><br>
 <?php
-    // Nome playlist 
-    // Nome musica
-    // Nome banda
-    // Nome Genero
-    // URL com iframe
     include "conexao.php";
 
     $select = "SELECT * FROM playlist ORDER BY nome";
@@ -23,36 +27,12 @@
         $res2 = mysqli_query($con, $select2);
         echo "<b>".$row["nome"]."</b><br><br>";
         while($row2 = mysqli_fetch_assoc($res2)){
-            echo $row2["nome_musica"]."(".$row2["nome_banda"].") <br> ".$row2["nome_genero"]." <br> URL: ".$row2["url_youtube"]." <br>";
+            echo $row2["nome_musica"]."(".$row2["nome_banda"].") <br> ".$row2["nome_genero"]." <br> <iframe width='1280' height='480' src='https://www.youtube.com/embed/".$row2["url_youtube"]."' frameborder='0' allow='accelerometer'; autoplay; clipboard-white; encrypted-media; gyroscope; picture-in-picture; allowfullscreen></iframe><br>";
             echo "<br>";
         }
         echo "<hr>"; 
     } 
-    
-    //$select2 = "SELECT * FROM musica_playlist INNER JOIN playlist ON musica_playlist.cod_playlist = playlist.id_playlist WHERE cod_playlist = '1'"
-
-
-
-
-
-
-
-    /*
-        $select = "SELECT playlist.nome as nome_playlist, musica.nome as nome_musica, banda.nome as nome_banda, 
-               genero.nome as nome_genero, musica.youtube as url_youtube FROM musica_player 
-               INNER JOIN playlist ON musica_playlist.cod_playlist=playlist.id_playlist
-               INNER JOIN musica ON playlist_musica.cod_musica=musica.id_musica
-               INNER JOIN banda ON musica.cod_banda=banda.id_banda 
-               INNER JOIN genero ON banda.cod_genero=genero.id_genero 
-               ORDER BY playlist.nome, musica.nome, banda.nome, genero.nome";
-    $res = mysqli_query($con, $select);
-    while($row = mysqli_fetch_array($res)){
-        echo "<b>".$row["nome_playlist"]."</b> - ";
-        echo "<b>".$row["nome_banda"]."</b> - ";
-        echo "<b>".$row["nome_genero"]."</b> |";
-        echo "<b>".$row["nome_musica"]."</b> - ";
-        echo "<b>".$row["nome_banda"]."</b> - ";
-        echo "<b>".$row["nome_genero"]."</b> |";
-    }
-    */
 ?>
+
+</body>
+</html>
