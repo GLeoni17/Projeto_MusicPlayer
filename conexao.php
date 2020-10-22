f875fdde9c2df1209f424b4f40130ca7b7c095ef
GLeoni17 <gabriel.lduarte@outlook.com>
2020-10-22 17:02:34 -0300

    Editado conexao.php - Conexao com o Banco de dados

--- conexao.php
+++ conexao.php
@@ -1,9 +1,8 @@
 <?php
-    $host = "localhost";
-    $user = "root";
-    $password = "usbw";
-    $db = "musicplayer";
-    if (!$con = mysqli_connect($host, $user, $password, $db)){
-        echo "Erro ao conectar";
-    }
+    $local = "db4free.net:3306";
+    $user = "gleoni";
+    $senha = "usbwusbw";
+    $bd = "glmusicplayer";
+
+    $con = mysqli_connect($local,$user,$senha,$bd) or die("erro");
 ?>
