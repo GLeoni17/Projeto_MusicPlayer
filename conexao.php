be52eedb0f3d0ee4d1adf24adedf4b0cf7d1268e
GLeoni17 <gabriel.lduarte@outlook.com>
2020-10-22 22:35:09 -0300

    Editado style do listar_playlist

--- conexao.php
+++ conexao.php
@@ -1,4 +1,11 @@
 <?php
+/*$local = "localhost";
+$user = "root";
+$senha = "usbw";
+$bd = "glmusicplayer";
+
+$con = mysqli_connect($local,$user,$senha,$bd) or die("erro");
+*/
     $local = "db4free.net:3306";
     $user = "gleoni";
     $senha = "usbwusbw";
