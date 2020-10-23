80f53b2cde99c1ff8a25987f09355dc05b0c66d3
GLeoni17 <gabriel.lduarte@outlook.com>
2020-10-23 17:18:08 -0300

    Conexao errada banco

--- conexao.php
+++ conexao.php
@@ -1,15 +1,15 @@
 <?php
-$local = "localhost";
+/*$local = "localhost";
 $user = "root";
 $senha = "usbw";
 $bd = "glmusicplayer";
 
-$con = mysqli_connect($local,$user,$senha,$bd) or die("erro");
-/*
+$con = mysqli_connect($local,$user,$senha,$bd) or die("erro");*/
+
    $local = "db4free.net:3306";
     $user = "gleoni";
     $senha = "usbwusbw";
     $bd = "glmusicplayer";
 
-    $con = mysqli_connect($local,$user,$senha,$bd) or die("erro");*/
+    $con = mysqli_connect($local,$user,$senha,$bd) or die("erro");
 ?>
